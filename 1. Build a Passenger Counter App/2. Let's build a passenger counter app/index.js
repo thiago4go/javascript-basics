360448bd8219c9cc06ef0e9e1d9fffc7fac236ab
Thiago S Shimada <113816194+thiago4go@users.noreply.github.com>
2023-01-16 02:12:14 +0000

    Completed the First js It counts, increments and saves

--- 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
+++ 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
@@ -1,1 +1,35 @@
-        document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+//let myAge = 33
+//let humanDogRatio = 6
+//let myDogAge = myAge*humanDogRatio
+//console.log(myDogAge)
+// initizlize the count as 0
+/* listen for clicks on the increment button.
+*/
+
+// grab the element and store it in a variable called countEl
+let countEL =  document.getElementById("count-el")
+let countSave = document.getElementById("count-save")
+let count = 0
+let previousEnt = document.getElementById("save-el")
+/* this function increment the number in count by 1 after each click, and shows the new
+results at the element using the sintex .innerText
+*/
+function increment(){
+    count += 1 
+    countEL.innerText = count
+}
+
+
+/* this funtion save number of entries and print it 
+then, it set the count as zero
+*/
+function save(){
+    countSave = count
+    previousEnt.textContent +=  countSave + " - " 
+    count = 0
+    countEL.innerText = count
+}
+
+
+
