8447fcd3af531c40cbf3ee127517f0c97efbddbc
Thiago S Shimada <113816194+thiago4go@users.noreply.github.com>
2023-01-18 06:58:51 +0000

    day1

--- 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,15 @@
 // 1. Create two variables, firstCard and secondCard. 
+let firstCard
+let secondCard
+
 // Set their values to a random number between 2-11
+firstCard = 4
+secondCard = 11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+
+let sum
+sum = firstCard + secondCard
+
+console.log(sum)
+
--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,3 +2,10 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("Do you want to draw another card")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack")
+} else  {
+    console.log("You're out of the game")
+}
